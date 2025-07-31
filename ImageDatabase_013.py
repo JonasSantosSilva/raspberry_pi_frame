611f829b67fba607ee82798dc0796c3ef59cafb7
Jonas Dos Santos Silva <eng.jonas.s@gmail.com>
2025-08-01 09:33:00 +1000

    Add settings menu and config file persistence

--- ImageDatabase_013.py
+++ ImageDatabase_013.py
@@ -2,8 +2,11 @@
 ## Title: Digital photo frame application
 ## Autor: Jonas dos Santos Silva
 ## Date: 09/2021
-## Release notes: Location button implemented
-##                Fixed the bug of returning to main menu from the location function
+## Release notes: Removed Fullscreen and Exit buttons from pictutre menu
+##                Implemented read config file
+##                Implemented write config file
+##                Implemented settings menu
+##                Implemented "settings_menu_destroy" function
 ##                
 #######################################################################################################################
 
@@ -30,7 +33,7 @@ image_3_location = "Images/background_example.jpeg"
 img_settingsbutton_location = "Images/Settings_Icon_6.png"
 img_picturesbutton_location = "Images/pics_icon_1.png"
 img_exitbutton_location = "Images/exit_icon_3.png"
-img_locationbutton_location = "Images/location_icon_1.png" #
+img_locationbutton_location = "Images/location_icon_1.png"
 image_backbutton_location = 'Images/right_transparent_arrow.png'
 image_forwardbutton_location = 'Images/right_transparent_arrow.png'
 
@@ -38,6 +41,7 @@ cmd_py_location = '/home/pi/Documents/Python/Projects/GPS_0.3.py'
 
 vertical_menu_offset = 30 # Offset to fit the image menu
 current_screen = 0 # 0 Main menu , 1 Image menu, 2 Image full screen, 3 My location, 4 Settings
+pic_change_delay_ms = 3000 # Time bewtween chamging pictures in fullscreen mode | This value might be replaced by the Config file
 
 
 ############################### Definitions - Classes #################################################################
@@ -78,17 +82,25 @@ def resizeimage_to_fit(image, fit_width, fit_height, save=False, temp_menuImg=No
 
 
 def forward():
-    global img_index, list_len
+    global img_index, list_len, current_screen
     if img_index == list_len-1: img_index = 0
     else: img_index += 1
-    image_put()
-        
-        
+
+    if current_screen == 1: # 1 Image menu
+        pic_menu_destroy(1)
+    elif current_screen == 2: # 2 Image full screen
+        image_destroy(2)
+
+
 def back():
-    global img_index, list_len
+    global img_index, list_len, current_screen
     if img_index == 0: img_index = list_len-1
     else: img_index -= 1
-    image_put()
+    
+    if current_screen == 1: # 1 Image menu
+        pic_menu_destroy(1)
+    elif current_screen == 2: # 2 Image full screen
+        image_destroy(2)
 
 
 def fullscreen_toggle():
@@ -105,9 +117,7 @@ def image_auto_update():
 def call_location_script():
     global current_screen
     current_screen = 0
-    print("ImageDatabase_012.py - call_location_script - start")
     GPS_03.main()
-    print("ImageDatabase_012.py - call_location_script - end")
     main()
 #     global subprocess
 #     p = subprocess.Popen(cmd_py_location, shell=True)
@@ -133,49 +143,39 @@ def image_put():
         if last_screen == 2: root.after_cancel(img_loop)
     elif current_screen == 2: # 2 Image full screen
         curr_image = resizeimage_to_fit(Image.open(image_list[img_index]), root.winfo_screenwidth(), root.winfo_screenheight())
-        img_loop = root.after(3000, image_auto_update)
+        img_loop = root.after(pic_change_delay_ms, image_auto_update)
     else: return 1
     
     curr_image_button = Button(root, image=curr_image, command=fullscreen_toggle, cursor='hand2')
-    curr_image_button.grid(row=0, columnspan=5, sticky="ew")
+    curr_image_button.grid(row=0, columnspan=3, sticky="nsew")
 
 
 def pic_menu_destroy(next_screen): # next_screen -> 0 Main menu , 1 Image menu, 2 Image full screen, 3 My location, 4 Settings
-    global list_mainmenu_buttons
+    global pic_menu_buttons
     
-    all_buttons_list = side_buttons
-    all_buttons_list.extend(middle_buttons)
-    for button in all_buttons_list:
+    for button in pic_menu_buttons:
         button.destroy()
     image_destroy(next_screen)
 
 
 def pic_menu_put():
-    global root, vertical_menu_offset, backbutton, fullscreenbutton, menu_exitbutton, menubutton, forwardbutton, side_buttons, middle_buttons
+    global root, vertical_menu_offset, pic_menu_buttons,  backbutton, menubutton, forwardbutton
+    
     #image_forwardbutton = PhotoImage(file=image_forwardbutton_location).subsample(button_subsample_factor, button_subsample_factor)
     #image_backbutton = PhotoImage(file=image_backbutton_location).subsample(button_subsample_factor, button_subsample_factor)
 
     #backbutton = Button(root, image=image_backbutton, command=thing2, borderwidth=0, cursor='hand2')
-    backbutton = Button(root, text = "<< Back", command=back, cursor='hand2')
-    fullscreenbutton = Button(root, text = "Full Screen", command=lambda: pic_menu_destroy(2), cursor='hand2')
-    menu_exitbutton = Button(root, text = "Exit Program", command=root.destroy, cursor='hand2')
-    menubutton = Button(root, text = "Menu", command=lambda: pic_menu_destroy(0), cursor='hand2')
-    #forwardbutton = Button(root, image=image_forwardbutton, command=thing1, borderwidth=0, cursor='hand2')
-    forwardbutton = Button(root, text = "Forward >>", command=forward, cursor='hand1')
+    backbutton = Button(root, text = "<< Back", command=back, font=("Helvetica", 42), cursor='hand2')
+    menubutton = Button(root, text = "Menu", command=lambda: pic_menu_destroy(0), font=("Helvetica", 42), cursor='hand2')
+    forwardbutton = Button(root, text = "Forward >>", command=forward, font=("Helvetica", 42), cursor='hand1')
 
-    side_buttons = [backbutton, forwardbutton]
-    middle_buttons = [fullscreenbutton, menu_exitbutton, menubutton]
+    pic_menu_buttons = [backbutton, menubutton, forwardbutton]
 
-    Grid.rowconfigure(root, index=0, weight=2) # index is the row number
-    Grid.columnconfigure(root, index=0, weight=2)
-    Grid.columnconfigure(root, index=4, weight=2)
-    backbutton.grid(row=1, column=0, sticky="ew", padx=1, pady=1)
-    forwardbutton.grid(row=1, column=4, sticky="ew", padx=1, pady=1)
-        
-    Grid.rowconfigure(root, index=0, weight=1) # index is the row number   
-    for index_b, button in enumerate(middle_buttons):
-        Grid.columnconfigure(root, index=index_b+1, weight=1)
-        button.grid(row=1, column=index_b+1, sticky="ew", padx=1, pady=1)
+    Grid.rowconfigure(root, index=0, weight=1) # index is the row number
+    Grid.rowconfigure(root, index=1, weight=1) # index is the row number
+    for index_b, button in enumerate(pic_menu_buttons):
+        Grid.columnconfigure(root, index=index_b, weight=1)
+        button.grid(row=1, column=index_b, sticky="ew", padx=1, pady=1)
 
 
 def pictures_menu():
@@ -200,6 +200,81 @@ def load_mainmenu_images():
     img_locationbutton_tk = resizeimage_to_fit(Image.open(list_mainmenu_imgs_locations[1]), (win_width/2), (win_height/2), True, img_locationbutton)   
     img_picturesbutton_tk = resizeimage_to_fit(Image.open(list_mainmenu_imgs_locations[2]), (win_width/2), (win_height/2), True, img_picturesbutton)
     img_exitbutton_tk = resizeimage_to_fit(Image.open(list_mainmenu_imgs_locations[3]), (win_width/2), (win_height/2), True, img_exitbutton)   
+
+
+def settings_menu_destroy(next_screen):
+    global current_screen, list_settings_menu_widgets
+
+    for widget in list_settings_menu_widgets:
+        widget.destroy()
+    
+    current_screen = next_screen
+    main()
+
+
+def settings_menu():
+    global list_settings_menu_widgets
+    
+    radio_input= IntVar() # Declaring Tkinter variable (Not: use "radio_input.get()" to read the variable)
+    
+    title_pic = Label(root, text="PICTURES", font=("Helvetica", 24))
+    title_pic.pack(pady=20)
+    title_pic_delay = Label(root, text="Auto change delay:", font=("Helvetica", 16))
+    title_pic_delay.pack(pady=10)
+    
+    delays = [("2s", "2000"),
+              ("3s", "3000"),
+              ("4s", "4000"),
+              ("8s", "4000"),
+              ("16s", "8000")]
+
+    delay = StringVar()
+    delay.set("3000")
+    
+#     for text, time_ms in delays: # Declaring Radio Buttons
+#         Radiobutton(root, text=text, variable=delay, value=time_ms, font=("Helvetica", 16)).pack(pady=10)
+        
+    settings_2s = Radiobutton(root, text="2s", variable=delay, value=2000, font=("Helvetica", 16))
+    settings_2s.pack(pady=10)
+    settings_3s = Radiobutton(root, text="3s", variable=delay, value=3000, font=("Helvetica", 16))
+    settings_3s.pack(pady=10)
+    settings_4s = Radiobutton(root, text="4s", variable=delay, value=4000, font=("Helvetica", 16))
+    settings_4s.pack(pady=10)
+    settings_8s = Radiobutton(root, text="8s", variable=delay, value=8000, font=("Helvetica", 16))
+    settings_8s.pack(pady=10)
+    settings_16s = Radiobutton(root, text="16s", variable=delay, value=16000, font=("Helvetica", 16))
+    settings_16s.pack(pady=10)
+
+    settings_apply_button =  Button(root, text = "Apply", command=lambda: settings_file_write(delay.get()), font=("Helvetica", 16))
+    settings_mainmenu_button = Button(root, text = "Main Menu", command=lambda: settings_menu_destroy(0), font=("Helvetica", 16), cursor='hand2')
+    settings_apply_button.pack(pady=10)
+    settings_mainmenu_button.pack(pady=10)
+    
+    list_settings_menu_widgets = [title_pic, title_pic_delay, settings_apply_button, settings_mainmenu_button,
+                                  settings_2s, settings_3s, settings_4s, settings_8s, settings_16s]
+
+
+def settings_file_write(delay_value):
+    global pic_change_delay_ms
+    pic_change_delay_ms = delay_value
+    try:
+        # Here it should read config.txt and call the "config" parser
+        with open('Config.txt','w') as config:
+            config.write(delay_value)
+    except:
+        print("Error: Could not write in Config.txt")
+
+
+def settings_file_read_apply():
+    global pic_change_delay_ms
+    try:
+        with open('Config.txt','r') as config:
+            config_content = config.read()
+            # Here should call the config parser
+            pic_change_delay_ms = int(config_content)
+            print("settings_file_read_apply - pic_change_delay_ms", pic_change_delay_ms)
+    except:
+        print("WARNING: Config file not found, standard settings applied.")
 
 
 def main_menu_destroy(next_screen):
@@ -238,11 +313,14 @@ def main():
         image_put()
     elif current_screen == 3: # 1 Image menu, 2 Image full screen, 3 My location, 4 Settings        
         call_location_script()
+    elif current_screen == 4: # 1 Image menu, 2 Image full screen, 3 My location, 4 Settings        
+        settings_menu()
     root.mainloop()# Main Loop
 
 
 def load_files():
     load_mainmenu_images()
+    settings_file_read_apply()
     
 
 ############################### Definitions - Global Variables #######################################################
@@ -266,5 +344,6 @@ if __name__ == "__main__":
 #        ser.close()
 #        f.close()
         print('Program Closed')
+
 
 
